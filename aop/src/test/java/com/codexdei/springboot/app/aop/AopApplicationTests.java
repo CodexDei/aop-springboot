c5d0772ac16fe79382d8294f6d87db862f53f56d
codexdei <jlacostc@gmail.com>
2025-12-23 06:46:03 -0500

    se crea proyecto aop

---
+++ aop/src/test/java/com/codexdei/springboot/app/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexdei.springboot.app.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
